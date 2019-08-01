324f197072a205a3e7bd50f18e85d05c8fa3233c
ghiffariarw <callmeghif@gmail.com>
2019-08-01 14:52:04 +0700

    movie list and detail

--- controller/c_movie.js
+++ controller/c_movie.js
@@ -1,15 +1,54 @@
-const {Movie} = require('../models/')
-class ControllerMovie{
-    static showMovie(req,res){
+const {
+    Movie,
+    DescMovie
+} = require('../models/')
+
+class ControllerMovie {
+
+    static showMovie(req, res) {
         Movie.findAll({
-            order : [['id','ASC']]
-        })
-        .then((data)=>{
-            res.send(data)
-        })
-        .catch(err=>{
-            res.send(err)
-        })
+                order: [
+                    ['id', 'ASC']
+                ]
+            }, {
+                include: {
+                    model: DescMovie
+                }
+            })
+            .then((movies) => {
+                res.render('movie/movies', {
+                    movies
+                })
+                // res.send(movies)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    static detail(req, res) {
+        Movie.findByPk(req.params.id, {
+                include: {
+                    model: DescMovie
+                }
+            })
+            .then(movie => {
+                // res.send(movie)
+                res.render('movie/detail', {
+                    movie
+                })
+            })
+            .catch(err => console.log(err))
+    }
+
+    static showSeats(req, res) {
+        Movie.findByPk(req.params.id)
+            .then(dataMovie => {
+                res.render('movie/seats', {
+                    dataMovie
+                })
+            })
+            .catch(err => console.log(err))
     }
 
 }
--- routes/r_movie.js
+++ routes/r_movie.js
@@ -2,5 +2,7 @@ const routes = require('express').Router()
 const Controller = require('../controller/c_movie')
 
 routes.get('/',Controller.showMovie)
+routes.get('/:id', Controller.detail)
+routes.get('/:id/seats', Controller.showSeats)
 
 module.exports = routes
